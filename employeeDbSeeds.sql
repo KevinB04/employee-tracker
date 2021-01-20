99a4d44f7d642acf1906a41a68c5a63c17c16894
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-20 10:02:12 -0500

    troubleshooting code in Seeds.sql and index.js

--- employeeDbSeeds.sql
+++ employeeDbSeeds.sql
@@ -7,7 +7,7 @@ USE employeeTracker_DB;
 CREATE TABLE department (
 	id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-	PRIMARY KEY (id),
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
@@ -15,7 +15,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
@@ -24,5 +24,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require("inquirer");
 const connection = mysql.createConnection({
     host: "localhost",
 
-port: 3000,
+port: 3306,
 
 user: "root",
 
@@ -12,19 +12,20 @@ password: "RememberThis!4",
 database: "employeeTracker_db",
 });
 
-connection.connect((err) => {
+connection.connect(function (err) {
     if (err) throw err;
     console.log(`connected as id: ${connection.threadId}`);
-    init();
+    // init();
+    postItemForAuction();
   });
 
   function postItemForAuction() {
       inquirer.prompt([
         {
-            type: "input",
+            type: "list",
             message: "What would you like to do?",
             name: "action",
-            choice: [
+            choices: [
                 "View All Employees", 
                 "View All Employees By Department", 
                 "View All Employees By Manager", 
@@ -32,14 +33,21 @@ connection.connect((err) => {
                 "Remove Employee", 
                 "Update Employee Role", 
                 "Update Employee Manager"],
-        },
-        {
-            type: "input",
-            message: "Which?",
-            name: "bid"
-        },
-//     ]).then(({name, bid}) => {
-//         connection.query(
+        }
+    ]).then(({action, bid}) => {
+        if(action === "View All Employees") {
+            viewAllEmployees();
+        }
+        console.table(action); 
+
+        });
+    };
+
+    function viewAllEmployees(){
+        
+    }
+        connection.query(
+
 //             `INSERT INTO items (name, bid)
 //             VALUES (?, ?);`,
 //             [name, bid],
@@ -64,16 +72,16 @@ connection.connect((err) => {
 //       );
 //   }
 
-//   function init() {
-//     inquirer
-//       .prompt([
-//         {
-//             type: "list",
-//             message: "What would you like to do?",
-//             name: "action",
-//             choices: ["ADD AN ITEM FOR AUCTION", "EXIT"],
-//         },
-//       ]).then(({ action }) => {
+  function init() {
+    inquirer
+      .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "action",
+            choices: [
+        },
+      ]).then(({ action }) => {
 //           if (action === "Post an item") {
 //               readOptionlist();
 //           } else if (action === "Bid on an item") {
