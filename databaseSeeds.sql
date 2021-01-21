7167773d52ad9bde82726665aab7e75794488e72
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-21 11:22:31 -0500

    adding databaseSeeds.sql and updating index.js

---
+++ databaseSeeds.sql
@@ -0,0 +1,23 @@
+USE employeeTracker_DB;
+
+INSERT INTO departments (name);
+VALUES ("Management");
+INSERT INTO departments (name);
+VALUES ("Legal");
+INSERT INTO departments (name);
+VALUES ("Engineering");
+INSERT INTO departments (name);
+VALUES ("Marketing");
+INSERT INTO departments (name);
+VALUES ("Human Resources");
+
+INSERT INTO employee (first name, last name, id, department);
+VALUES  ("James", "Bradley", 57, "Managment");
+INSERT INTO employee (first name, last name, id, department);
+VALUES ("Ashley", "Brisbon", 104, "Legal");
+INSERT INTO employee (first name, last name, id, department);
+VALUES ("Neil", "Brown", 117, "Engineering");
+INSERT INTO employee (first name, last name, id, department);
+VALUES ("Shaun", "Oppedisano", 95, "Marketing");
+INSERT INTO employee (first name, last name, id, department);
+VALUES ("Erin", "Kelly", 125, "Human Resources");
--- index.js
+++ index.js
@@ -2,66 +2,100 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-port: 3306,
+  port: 3306,
 
-user: "root",
+  user: "root",
 
-password: "RememberThis!4",
-database: "employeeTracker_db",
+  password: "RememberThis!4",
+  database: "employeeTracker_db",
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log(`connected as id: ${connection.threadId}`);
-    // init();
-    postItemForAuction();
-  });
+  if (err) throw err;
+  console.log(`connected as id: ${connection.threadId}`);
+  // init();
+  postItemForAuction();
+});
 
-  function postItemForAuction() {
-      inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            name: "action",
-            choices: [
-                "View All Employees", 
-                "View All Employees By Department", 
-                "View All Employees By Manager", 
-                "Add Employee", 
-                "Remove Employee", 
-                "Update Employee Role", 
-                "Update Employee Manager"],
-        }
-    ]).then(({action, bid}) => {
-        if(action === "View All Employees") {
-            viewAllEmployees();
-        }
-        console.table(action); 
-
-        });
-    };
-
-    function viewAllEmployees(){
-        
+function postItemForAuction() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "action",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager"],
     }
-        connection.query(
+  ]).then(({ action, bid }) => {
+    if (action === "View All Employees") {
+      viewAllEmployees();
+    }
+    console.table(action);
 
-//             `INSERT INTO items (name, bid)
-//             VALUES (?, ?);`,
-//             [name, bid],
-//             (err, data) => {
-//                 if (err) throw err;
-//                 console.log(data);
-//                 init();
-//             }
-//         );
-//     });
-// }
+  });
+};
 
+function viewAllEmployees() {
 
-    
+}
+connection.query(
+
+  //             `INSERT INTO items (name, bid)
+  //             VALUES (?, ?);`,
+  //             [name, bid],
+  //             (err, data) => {
+  //                 if (err) throw err;
+  //                 console.log(data);
+  //                 init();
+  //             }
+  //         );
+  //     });
+  // }
+
+  function init() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What would you like to search for?",
+          choices: [
+            "Songs by Artist",
+            "Songs by Title",
+            "Artists appearing more than once",
+            "Find songs by year",
+            "Exit",
+          ],
+          name: "userChoice",
+        },
+      ])
+      .then(({ userChoice }) => {
+        switch (userChoice) {
+          case "Songs by Artist":
+            findSongByArtist();
+            break;
+          case "Songs by Title":
+            findSongByTitle();
+            break;
+          case "Artists appearing more than once":
+            findArtistsMoreThanOnce();
+            break;
+          case "Find songs by year":
+            findSongsByYearRange();
+            break;
+          default:
+            exit();
+        }
+      });
+  }
+
 //       connection.query(
 //           `INSERT INTO items (name, bid)
 //           VALUES ("Football", 10);`,
@@ -76,10 +110,10 @@ connection.connect(function (err) {
     inquirer
       .prompt([
         {
-            type: "list",
-            message: "What would you like to do?",
-            name: "action",
-            choices: [
+          type: "list",
+          message: "What would you like to do?",
+          name: "action",
+          choices: [
         },
       ]).then(({ action }) => {
 //           if (action === "Post an item") {
